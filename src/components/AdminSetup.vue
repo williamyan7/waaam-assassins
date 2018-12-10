dba3ab700405eef89b5855a8768f2010cedf9b2f
williamyan7 <williamyan7@gmail.com>
2018-12-09 23:30:35 -0500

    Updated initial team target setting

--- src/components/AdminSetup.vue
+++ src/components/AdminSetup.vue
@@ -13,7 +13,9 @@
     <br><br>Enter number of teams per dynasty: <input class="inputField" type="text" name="numTeams" v-model="num_teams"></input>
     <button class="btn red" @click="groupTeams">Group Teams</button>
     <button class="btn red" @click="pushTeamAssignmentToCloud">Update Teams</button>
-    <button class="btn red" @click="assignInitialTargets">Assign Initial Teams</button>
+    <br><button class="btn red" @click="createTeamObjects">Create Team Objects</button>
+    <button class="btn red" @click="deleteTeams">Delete Team Objects</button>
+    <br><button class="btn red" @click="assignInitialTargets">Assign Initial Targets</button>
   </div>
 </template>
 
@@ -187,16 +189,102 @@ export default {
       }
     },
     createTeamObjects() {
-      //Create team objects to be updated in Firebase based on team number
-
+      //Loop for Fire dynasty
+      for(var i=0; i<this.num_teams; i++) {
+        var team_num = (10+i).toString()
+        firebase.firestore().collection('teams').doc(team_num).set({
+          accepted_kill_codes: [],
+          dynasty: 'Fire',
+          status: 'Alive',
+          team_number: team_num,
+          target_team: null,
+          targeted_by_team: null
+        })
+      }
+      //Loop for water dynasty
+      for(var i=0; i<this.num_teams; i++) {
+        var team_num = (20+i).toString()
+        firebase.firestore().collection('teams').doc(team_num).set({
+          accepted_kill_codes: [],
+          dynasty: 'Water',
+          status: 'Alive',
+          team_number: team_num,
+          target_team: null,
+          targeted_by_team: null
+        })
+      }
+      for(var i=0; i<this.num_teams; i++) {
+        var team_num = (30+i).toString()
+        firebase.firestore().collection('teams').doc(team_num).set({
+          accepted_kill_codes: [],
+          dynasty: 'Earth',
+          status: 'Alive',
+          team_number: team_num,
+          target_team: null,
+          targeted_by_team: null
+        })
+      }
+      for(var i=0; i<this.num_teams; i++) {
+        var team_num = (40+i).toString()
+        firebase.firestore().collection('teams').doc(team_num).set({
+          accepted_kill_codes: [],
+          dynasty: 'Air',
+          status: 'Alive',
+          team_number: team_num,
+          target_team: null,
+          targeted_by_team: null
+        })
+      }
+    },
+    deleteTeams() {
+      firebase.firestore().collection('teams').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          var id = doc.data().team_number.toString()
+          firebase.firestore().collection('team').doc(id).delete()
+        })
+      })
     },
     updateAcceptedKillCodes() {
       //updates the team objects in Firebase with acceptable kill codes
     },
     assignInitialTargets() {
-      firebase.firestore().collection('teams')
-      // Create 4 arrays with the 4 dynasty Teams
-
+      var teams_array = []
+      firebase.firestore().collection('teams').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          //Assign target teams by checking dynasty
+          var team_num = doc.data().team_number
+          var HIGHEST_TEAM_NUM = 42
+          if(doc.data().dynasty == "Air") {
+            if(doc.data().team_number == HIGHEST_TEAM_NUM) {
+              firebase.firestore().collection('teams').doc(team_num.toString()).update({
+                target_team: 10
+              })
+            } else {
+                firebase.firestore().collection('teams').doc(team_num.toString()).update({
+                  target_team: (team_num - 30 + 1)
+                })
+              }
+          } else {
+            firebase.firestore().collection('teams').doc(team_num.toString()).update({
+              target_team: (parseInt(team_num,10) + 10)
+            })
+          }
+        })
+      }).then(this.setTargetedTeam())
+    },
+    setTargetedTeam() {
+      firebase.firestore().collection('teams').get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          var current_team_num = doc.data().team_number
+          var target_team_num = doc.data().target_team
+          firebase.firestore().collection('teams').doc(target_team_num.toString()).update({
+            targeted_by_team: current_team_num
+          })
+        })
+      })
     },
     shuffleArray(array) {
       console.log('in the shuffle')
